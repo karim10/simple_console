a7cc989a1ae9247b44dcc4e5ed2fd0d35a87c0be
KARIM BEN YEZZA <karim.b.yezza@gmail.com>
2022-08-29 17:29:55 +0200

    test

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const path = require('path')
-const app = express()
+const PORT = process.env.PORT || 5000
 
-app.use(express.static(path.join(__dirname, './client/build')))
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build', 'index.html'))
-})
-
-const port = process.env.PORT || 8000
-
-app.listen(port, () => {
-    console.log(`listening on port ${3000}`)
-})
+express()
+    .use(express.static(path.join(__dirname, 'client/build')))
+    .get('/', (req, res) => res.render('index'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
